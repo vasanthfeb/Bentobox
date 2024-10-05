83347226b64b098847eee448928631ddbc183170
vasanth <70842484+vasanthfeb@users.noreply.github.com>
2024-10-05 11:38:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    // Toggle menu on click
+    $("#menu-toggler").click(function() {
+      toggleBodyClass("menu-active");
+    });
+  
+    function toggleBodyClass(className) {
+      document.body.classList.toggle(className);
+    }
+  
+   });
+
+
+
+   //product page 
+   function addToCart(name, price) {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    const existingItem = cart.find(item => item.name === name);
+
+    if (existingItem) {
+        existingItem.quantity++;
+    } else {
+        cart.push({ name, price, quantity: 1 });
+    }
+
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert(`${name} added to cart!`);
+}
